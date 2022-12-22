4fe49ab22d37ec33c66b3083c8c17f0b7d36e994
magican09 <maga26497@mail.ru>
2022-12-22 18:14:07 +0300

    gg

--- bldCustomControlLibrary/CustomControls/GanttDiargamDataGrid/GanttDiargamDataGrid.cs
+++ bldCustomControlLibrary/CustomControls/GanttDiargamDataGrid/GanttDiargamDataGrid.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace bldCustomControlLibrary
 {
     public class GanttDiargamDataGrid:DataGrid
     {
-        
+
+
+        public object ItemsSource
+        {
+            get { return (object)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemsSourse.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSourse", typeof(object), typeof(GanttDiargamDataGrid), new PropertyMetadata(new CoerceValueCallback(OnCoerceValueCallback)));
+
+
+
+        public object ConsructionProject
+        {
+            get { return (object)GetValue(ConsructionProjectProperty); }
+            set { SetValue(ConsructionProjectProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ConsructionProject.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ConsructionProjectProperty =
+            DependencyProperty.Register("ConsructionProject", typeof(object), typeof(GanttDiargamDataGrid), new PropertyMetadata(0));
+
+        public override void OnApplyTemplate()
+        {
+            
+            base.OnApplyTemplate();
+        }
+
     }
 }
